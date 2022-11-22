d744cfc36b02a28cb373976421b118a5a2cf178a
Kukuh Setya Nugraha <kakadz.pro@gmail.com>
2022-11-22 10:22:20 +0700

    add dependecy

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+// import './assets/main.css'
+import "./index.css";
 
-const app = createApp(App)
+// library fixed
+import Datepicker from "@vuepic/vue-datepicker";
+import VueGoodTablePlugin from "vue-good-table-next";
+import vSelect from "vue-select";
+import { QuillEditor } from "@vueup/vue-quill";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+const globalOptions = {
+  //   debug: "info",
+  //   modules: {
+  //     toolbar: "minimal",
+  //   },
+  placeholder: "Type here . . .",
+  readOnly: false,
+  theme: "snow",
+};
+
+app.use(createPinia());
+app.use(router);
+app.component("Datepicker", Datepicker);
+app.use(VueGoodTablePlugin);
+app.component("v-select", vSelect);
+app.component("QuillEditor", QuillEditor);
+
+app.mount("#app");
