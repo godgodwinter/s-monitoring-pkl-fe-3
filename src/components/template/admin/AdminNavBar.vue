20f271e42deaecb3e583cd95a780eb2aa2455fd2
Kukuh Setya Nugraha <kakadz.pro@gmail.com>
2022-11-23 15:53:16 +0700

    update menu

--- src/components/template/admin/AdminNavBar.vue
+++ src/components/template/admin/AdminNavBar.vue
@@ -45,7 +45,7 @@ window.addEventListener("scroll", onScroll);
           </button>
         </div>
 
-        <button v-if="theme == 'cupcake'" @click="doUpdateTheme('dark')"
+        <!-- <button v-if="theme == 'cupcake'" @click="doUpdateTheme('dark')"
           class="font-normal text-sm hover:link my-1 block capitalize px-4 border-r last:border-none">
           <svg class="h-6" viewBox="0 0 24 24" style="fill: none; stroke: currentcolor">
             <path stroke-linecap="round" stroke-linejoin="round"
@@ -59,7 +59,7 @@ window.addEventListener("scroll", onScroll);
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
           </svg>
-        </button>
+        </button> -->
         <div class="px-2">
           <label for="my-drawer-2" class="btn drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg"
               class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
